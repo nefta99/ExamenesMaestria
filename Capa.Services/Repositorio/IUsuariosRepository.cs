a18c796eb11e816f4e4be16e62779a394a2ccf81
nef <neftalitorres@itesm.mx>
2020-09-18 10:01:07 -0500

    desarrollo

---
+++ Capa.Services/Repositorio/IUsuariosRepository.cs
@@ -0,0 +1,15 @@
+﻿using Capa.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa.Services.Repositorio
+{
+    public interface IUsuariosRepository : IRepository<Usuarios>
+    {
+        Usuarios GetUsuariosPorId(int id);
+        int AgregarUsuario(string nombre, string pwd);
+    }
+}
